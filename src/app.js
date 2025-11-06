dbbce727e0bba4cc675b30a5006bca6cb74c5873
Benjamin Djaoui <benjamin.djaoui2@gmail.com>
2025-11-06 12:09:45 +0100

    Set author name (Benjamin DJAOUI)

--- src/app.js
+++ src/app.js
@@ -8,7 +8,7 @@
 const PROJECT_NAME = "git-collab-mini";
 
 // ★ À MODIFIER PAR LES DEUX (VALEUR DIFFÉRENTE OBLIGATOIRE)
-const AUTHOR_NAME = "Noah PRATS & Benjamin DJAOUI";
+const AUTHOR_NAME = "Benjamin DJAOUI";
 
 function buildBanner() {
 	return `[${PROJECT_NAME}] by ${AUTHOR_NAME}`;
